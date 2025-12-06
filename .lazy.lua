d0796e829406b1771afed26db5a977a595013418
Abhik Jain <abhik@abhikjain.xyz>
2025-12-06 15:29:21 +0100

    fix config

--- .lazy.lua
+++ .lazy.lua
@@ -1,27 +1,26 @@
 return {
 	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				rust_analyzer = {
-					settings = {
+		"mrcjkb/rustaceanvim",
+		config = function()
+			vim.g.rustaceanvim = {
+				server = {
+					default_settings = {
+						-- rust-analyzer language server configuration
 						["rust-analyzer"] = {
 							cargo = {
-								allFeatures = true,
-								-- "cfgs" tells the LSP to treat code guarded by
-								-- #[cfg(loomer)] as active/enabled.
-								cfgs = { "loomer" },
+								allFeatures = false,
+								noDefaultFeatures = true,
+								features = { "futures" },
 							},
-							-- Optional: If 'cargo check' (diagnostics) fails because it
-							-- doesn't know about the flag, you may also need this:
-							check = {
-								-- Passing the flag to the check command via RUSTFLAGS
-								extraEnv = { RUSTFLAGS = "--cfg loomer" },
+							checkOnSave = {
+								allFeatures = false,
+								noDefaultFeatures = true,
+								features = { "futures" },
 							},
 						},
 					},
 				},
-			},
-		},
+			}
+		end,
 	},
 }
